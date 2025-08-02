abca9731efed698bff08a389537190e09cb9cd90
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2025-08-02 15:07:42 -0700

    fix: Fake browser header to prevent Status Code 403 Edited Axios API to use a fake browser header, preventing Status Code 403 error upon search/GET requests

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://jisho.org/api/v1/search/'
+  baseURL: 'https://jisho.org/api/v1/search/',
+  timeout: 1000,
+  headers: { // Fake browser header to prevent Status Code 403
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36',
+    'Accept': 'application/json'
+  }
 })
